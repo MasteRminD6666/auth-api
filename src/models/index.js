d72a1e7cb6d04520c492c66622f569dc7d26bd09
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-11-10 13:50:50 +0200

    editing

--- src/models/index.js
+++ src/models/index.js
@@ -6,11 +6,20 @@ const food = require('./food/model');
 const clothes = require('./clothes/model');
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory;';
 
-const sequelize = new Sequelize(DATABASE_URL);
-
+const DATABASE_CONFIG =
+{
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+}
+const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
 module.exports = {
   db: sequelize,
   users: userModel(sequelize, DataTypes),
-  clothes:clothes(sequelize, DataTypes) , 
+  clothes: clothes(sequelize, DataTypes),
   food: food(sequelize, DataTypes),
 }
